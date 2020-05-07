10c37b2bb34de3e2b583d307638a6a1cdd79f527
kinokus <aleks.kinokus@gmail.com>
2020-05-07 16:44:46 +0300

    action

---
+++ advanced-angular-chat-client/src/app/actions/users.actions.ts
@@ -0,0 +1,6 @@
+// TODO in future - get active users
+export class GetAllUsers {
+  static readonly type = '[Users] Get All Users';
+  constructor() {
+  }
+}
--- advanced-angular-chat-client/src/app/states/users.state.ts
+++ advanced-angular-chat-client/src/app/states/users.state.ts
@@ -1,8 +1,10 @@
-import {Action, State, StateContext} from '@ngxs/store';
+import {Action, State, StateContext, Store} from '@ngxs/store';
 import User from '../../../../advanced-angular-chat-server/src/entities/User';
 import {Injectable} from '@angular/core';
 import {UserLogin} from '../actions/logged-user.actions';
 import {LoggedUser} from '../models/logged.user';
+import {GetAllUsers} from "../actions/users.actions";
+import {ApiService} from "../api.service";
 
 
 class ChatUsers {
@@ -20,8 +22,26 @@ class ChatUsers {
 @Injectable()
 export class UsersState {
   @Action(UserLogin)
-  userLogin(context: StateContext<LoggedUser>, action: UserLogin) {
+  userLogin(context: StateContext<ChatUsers>, action: UserLogin) {
     console.log(context);
     console.log(action);
   }
+
+  @Action(GetAllUsers)
+  getAllUsers(context: StateContext<ChatUsers>) {
+    this.apiService.getUsers().then(users => {
+      context.patchState({users});
+      const usersHelperById = {};
+      users.forEach(user => {
+        usersHelperById[user.id] = user.name;
+      });
+      context.patchState({usersHelperById});
+    });
+  }
+
+
+  constructor(private apiService: ApiService, private  store: Store) {
+  }
+
+
 }
